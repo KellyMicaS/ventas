51077a7c3bc552baf5cbde3f7e3811da2d0412c6
Kelly S <kellysolanodl@gmail.com>
2025-07-04 08:22:06 -0500

    Variables bases de datos

---
+++ config/global.php
@@ -0,0 +1,24 @@
+<?php
+//Ip a donde se va a conectar a la base de datos, le da un alias
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos, el alias se conecta con la de MySQL
+define("DB_NAME", "tienda");
+
+//Usuario de la base de datos
+define("DB_USER", "root");
+
+//Contraseña de la base de datos
+define("DB_PASS", "");
+
+//Definimos la codificacion de los caracteres /Qué tipo de caracteres o codificacion que se va a usar en la DB
+define("DB_ENCODE","utf8");
+
+//Definimos una constante con nombre del proyecto
+define("PRO_NOMBRE","SisVentas");
+
+
+
+
+
+?>
