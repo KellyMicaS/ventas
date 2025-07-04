6b81833378e58a9d7a564cb4b4edf4ae8033dff3
Kelly S <kellysolanodl@gmail.com>
2025-07-04 09:26:51 -0500

    Funciones BD

--- config/Conexion.php
+++ config/Conexion.php
@@ -20,5 +20,36 @@ if(mysqli_connect_errno())
     printf("Conexión exitosa a la base de datos: %s\n", DB_NAME);
 }
 
+//Definir un conjunto de funciones que nos ayude a la consulta de la BD 
+if(!function_exist('ejecutarConsulta')){
+    //$sql es para conectarse a la base de datos con CURP, select, insert, update, delete
+    function ejecutarConsulta($sql){
+        global= $conexion;
+
+        //Creo una variable para almacenar el resultado de la consulta
+        $query= $conexion->($sql);
+
+        //Retorno el resultado de la consulta
+        return $query;
+    }
+
+    //Creo una funcion que me permita obtener una sola fila de una tabla de la BD, recibe un parámetro
+    function ejecutarConsultaSimpleFila($sql){
+        //conectamos a la DB
+        global= $conexion;
+
+        //creo una variable para almacenar el resultado de la consulta
+        $query= $conexion->query($sql);
+
+        //obtengo una fila de la consulta, fetch_assoc es para seleccionar la fila
+        $row= $query->fetch_assoc();
+
+        //retorno a la fila obtenida
+        return $row;
+    }
+
+
+}
+
 
 ?>
